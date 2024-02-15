5b7f4a04f10bc639f5761df9f31f4c469ca00562
Zsolt <5047426+bodzaital@users.noreply.github.com>
2024-02-15 07:59:42 +0100

    Separate EXIF handling

---
+++ src/renderer/metadata/exif.js
@@ -0,0 +1,83 @@
+import Control from "../control";
+import { $ } from "../shorthand";
+
+class EXIF {
+	#placeholder = null;
+
+	#controls = {
+		shutter: $("#sidebarExifShutter"),
+		aperture: $("#sidebarExifAperture"),
+		iso: $("#sidebarExifIso"),
+		focalLength: $("#sidebarExifFocalLength"),
+		exposure: $("#sidebarExifExposureMode"),
+		cameraModel: $("#sidebarExifCamera"),
+		dateOnly: $("#sidebarExifDate"),
+	};
+
+	#tooltips = {
+		lensModel: null,
+		timeOnly: null,
+	};
+
+	constructor() {
+		this.#placeholder = new Control("div")
+			.class("placeholder")
+			.style("width", "100%")
+			.get();
+
+		this.#tooltips.timeOnly = new bootstrap.Tooltip($("#sidebarExifDateTime"));
+		this.#tooltips.lensModel = new bootstrap.Tooltip($("#sidebarExifLensModel"));
+
+		window.addEventListener("folderLoaded", () => this.loadFolder());
+		window.addEventListener("folderUnloaded", () => this.unloadFolder());
+	}
+
+	setData(shutter, aperture, iso, model, focalLength, dateTime, lensModel, exposure) {
+		this.#controls.shutter.innerText = shutter != null
+			? `${shutter}s`
+			: "N/A";
+
+		this.#controls.aperture.innerText = aperture != null
+			? `ƒ/${aperture}`
+			: "N/A";
+
+		this.#controls.iso.innerText = iso ?? "N/A";
+
+		this.#controls.cameraModel.innerText = model ?? "N/A";
+		this.#tooltips.lensModel.setContent({ ".tooltip-inner": lensModel ?? "N/A" });
+
+		this.#controls.focalLength.innerText = focalLength ?? "N/A";
+
+		this.#controls.dateOnly.innerText = dateTime != null
+			? dateTime.substring(0, 10).replaceAll(":", "-")
+			: "N/A";
+
+		const timeOnly = dateTime != null
+			? dateTime.substring(dateTime.indexOf(" "))
+			: "N/A";
+
+		this.#tooltips.timeOnly.setContent({ ".tooltip-inner": timeOnly ?? "N/A" });
+
+		this.#controls.exposure.innerText = exposure ?? "N/A";
+	}
+
+	clearData() {
+		for (const [key, value] of Object.entries(this.#controls)) {
+			this.#controls[key].innerText = "";
+			this.#controls[key].appendChild(this.#placeholder.cloneNode());
+		}
+
+		this.#tooltips.timeOnly.setContent({ ".tooltip-inner": "Unknown date and time" });
+		this.#tooltips.lensModel.setContent({ ".tooltip-inner": "Unknown lens model" });
+	}
+
+	loadFolder() {
+
+	}
+
+	unloadFolder() {
+		this.clearData();
+	}
+}
+
+export const exif = new EXIF();
--- src/renderer/sidebar.js
+++ src/renderer/sidebar.js
@@ -5,6 +5,7 @@ import { description } from './metadata/description';
 import { location } from './metadata/location';
 import { rating } from './metadata/rating';
 import { tagging } from './metadata/tagging';
+import { exif } from "./metadata/exif";
 import { $, $$ } from './shorthand';
 
 class Sidebar {
@@ -15,35 +16,13 @@ class Sidebar {
 
 	photoName = $("#photo-name");
 
-	#details = {
-		exifShutter: $("#sidebarExifShutter"),
-		exifAperture: $("#sidebarExifAperture"),
-		exifIso: $("#sidebarExifIso"),
-		exifCamera: $("#sidebarExifCamera"),
-		exifFocal: $("#sidebarExifFocalLength"),
-		exifDate: $("#sidebarExifDate"),
-		exifExposure: $("#sidebarExifExposureMode"),
-
-		placeholder: null,
-	};
-
-	#exifDateTimeTooltip = null;
-	#exifLensModelTooltip = null;
-
 	constructor() {
-		this.#details.placeholder = new Control("div")
-			.class("placeholder")
-			.style("width", "100%")
-			.get();
 		
 		this.#sidebarToggleButton.addEventListener("click", () => {
 			this.toggleSidebar(!this.#isSidebarOpen);
 			
 			window.invoke.saveSetting("isSidebarVisible", this.#isSidebarOpen);
 		});
-
-		this.#exifDateTimeTooltip = new bootstrap.Tooltip($("#sidebarExifDateTime"));
-		this.#exifLensModelTooltip = new bootstrap.Tooltip($("#sidebarExifLensModel"));
 
 		window.addEventListener("folderLoaded", () => {
 			this.loadFolder();
@@ -79,67 +58,23 @@ class Sidebar {
 		}
 	}
 
-	clearExifData() {
-		for (const [key, value] of Object.entries(this.#details)) {
-			if (key == "placeholder") continue;
-
-			this.#details[key].innerText = "";
-			this.#details[key].appendChild(this.#details.placeholder.cloneNode());
-		}
-
-		this.#exifDateTimeTooltip.setContent({ ".tooltip-inner": "Unknown date or time" });
-		this.#exifLensModelTooltip.setContent({ ".tooltip-inner": "Unknown lens" });
-	}
-
-	setExifData(shutter, aperture, iso, model, focal, dateTime, lensModel, exposure) {
-		this.#details.exifShutter.innerText = shutter != null
-			? `${shutter}s`
-			: "N/A";
-
-		this.#details.exifAperture.innerText = aperture != null
-			? `ƒ/${aperture}`
-			: "N/A";
-
-		this.#details.exifIso.innerText = iso ?? "N/A";
-
-		this.#details.exifCamera.innerText = model ?? "N/A";
-
-		this.#details.exifFocal.innerText = focal ?? "N/A";
-
-		const dateOnly = dateTime != null
-			? dateTime.substring(0, 10).replaceAll(":", "-")
-			: null;
-
-		dateTime = dateTime != null
-			? dateTime.substring(dateTime.indexOf(" "))
-			: null;
-
-		this.#details.exifDate.innerText = dateOnly ?? "N/A";
-
-		this.#details.exifExposure.innerText = exposure ?? "N/A";
-
-		this.#exifDateTimeTooltip.setContent({ ".tooltip-inner": dateTime ?? "N/A" });
-
-		this.#exifLensModelTooltip.setContent({ ".tooltip-inner": lensModel ?? "N/A" })
-	}
-
 	loadExifData(uri) {
-		this.clearExifData();
+		exif.clearData();
 
-		window.invoke.getExif(uri).then((exif) => {
-			const fnumber = exif.FNumber != null
-				? exif.FNumber?.value[0] / exif.FNumber?.value[1]
+		window.invoke.getExif(uri).then((data) => {
+			const fnumber = data.FNumber != null
+				? data.FNumber?.value[0] / data.FNumber?.value[1]
 				: null;
 
-			this.setExifData(
-				exif.ExposureTime?.description,
+			exif.setData(
+				data.ExposureTime?.description,
 				fnumber,
-				exif.ISOSpeedRatings?.description,
-				exif.Model?.description,
-				exif.FocalLength?.description,
-				exif.DateTime?.description,
-				exif.LensModel?.description,
-				exif.ExposureProgram?.description
+				data.ISOSpeedRatings?.description,
+				data.Model?.description,
+				data.FocalLength?.description,
+				data.DateTime?.description,
+				data.LensModel?.description,
+				data.ExposureProgram?.description
 			);
 		});
 	}
@@ -164,7 +99,6 @@ class Sidebar {
 	}
 
 	unloadFolder() {
-		this.clearExifData();
 		this.photoName.innerText = "No image";
 	}
 }
